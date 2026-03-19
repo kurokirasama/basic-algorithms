61a42107dac287c787324e175a7901884efd1945
kurokirasama <kurokirasama@gmail.com>
2026-03-19 18:25:09 -0300

    feat(algorithms): Implement 'sort_desc' logic and simulation page

--- src/algorithms.js
+++ src/algorithms.js
@@ -18,6 +18,28 @@ function minimo(x) {
   return min;
 }
 
+/**
+ * Sorts an array in descending order using selection sort.
+ * @param {number[]} x The input array.
+ * @returns {number[]} The sorted array.
+ */
+function sort_desc(x) {
+  const n = x.length;
+  for (let i = 0; i < n - 1; i++) {
+    let max_i = i;
+    for (let j = i + 1; j < n; j++) {
+      if (x[max_i] < x[j]) {
+        max_i = j;
+      }
+    }
+    const temp = x[i];
+    x[i] = x[max_i];
+    x[max_i] = temp;
+  }
+  return x;
+}
+
 module.exports = {
   minimo,
+  sort_desc,
 };
